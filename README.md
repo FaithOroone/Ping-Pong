bb369b88490bdd6109f1dabf19a61ba24567d218
faithoroone <faithorone59@gmail.com>
2018-07-02 10:06:13 +0300

    add a readme

---
+++ README.md
@@ -0,0 +1,29 @@
+# PING-PONG
+#### PIng-pong takes a number from a user and returns a range of numbers from 1 to the chosen number  01/07/2018
+#### By **Orone Faith Eunice**
+## Description
+The app takes in a number from a user and returns a range of numbers from 1 to the chosen number.A user is also able to enter a new number and see new results over and over again.
+## Specification
+* If the number is divisible by 3, ping is returned instead of the number.
+Example input: 3
+Example output: ping
+* If the number is divisible by 5, pong is returned instead of the number.
+Example input: 5
+Example output: pong
+* If the number is divisible by 15, pingpong is returned instead of the number.
+Example input: 15
+Example output: pingpong
+* If all the conditions are not met that is, number is not divisible by any of 3, 5,and 15, that particular number is returned.
+Example input: 2
+Example output: pingpong
+## Setup/Installation Requirements
+follow the link to clone the project on your computer then run it in any browser of your choice
+## Known Bugs
+No Bugs yet.
+## Technologies Used
+I used HTML, CSS, JQuery and Bootstrap
+## Support and contact details
+contact me on oronefaith@yahoo.com
+### License
+*{Determine the license under which this application can be used.  See below for more details on licensing.}*
+Copyright (c) 2018 **orone faith**
--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,7 @@
-//frontend logic
-$(document).ready(function(){
-  $("num-form").submit(function(event){
-    var user_input = parseInt($("NumberInput").val());
-
-    var Results = counter(user_input); //stores the input and creates a function
-    $("#numResult").empty(); //picks content from ul
-    Results.forEach(function(result){//adds results in a list
-      $("#numResult").append("<li>"+ result + "<li>");
-    });
-    event.preventDefault();
-  });
-});
-
 //backend logic
 var counter = function(UserInput){
   var numArray = [];
-  for (var number = 1; number <= UserInput;number +=1){// tests value and pushes text to index
+  for (var number = 1; number <= UserInput; number +=1){// tests value and pushes text to index
     if(number % 15 === 0){
       numArray.push("pingPong")
     }else if (number % 5 === 0){
@@ -27,3 +13,17 @@ var counter = function(UserInput){
     }
   }return numArray;
 }; //ends the function
+
+//frontend logic
+$(document).ready(function(){
+  $("#num-form").submit(function(event){
+    var user_input = parseInt($("#NumberInput").val());
+
+    var Results = counter(user_input); //stores the input and creates a function
+    $("#numResult").empty(); //picks content from ul
+    Results.forEach(function(result){//adds results in a list
+      $("#numResult").append("<li>"+ result + "</li>");
+    });
+    event.preventDefault();
+  });
+});
